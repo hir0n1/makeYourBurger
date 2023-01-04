1d5eab482d98c44f471901ff233a84439a31136a
hiron <jion.ru.08@mail.ru>
2023-01-04 17:15:52 +0300

    a

---
+++ dist/js/script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const   profileButton = document.querySelector('.header__profile')
+            profileMenu = document.querySelector('.profile-menu')
+            changeScreenBtn = document.querySelectorAll('.js-screen-btn')
+            screen = document.querySelectorAll('.js-screen')
+            headerBtns = document.querySelectorAll('.header__nav-btn')
+            
+    function showMenu(button, menu) {
+        button.addEventListener('click', () => {
+            menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+        });
+    };
+    
+    function changeScreen(button, screen) {
+        button.forEach((btn) => {
+           btn.addEventListener('click', () => {
+                if(btn.classList.contains('header__nav-btn_active')) {
+                    return;
+                }
+                headerBtns.forEach((item) => {
+                    if(item.classList.contains('header__nav-btn_active')) {
+                        item.classList.remove('header__nav-btn_active')
+                    } else {
+                        item.classList.add('header__nav-btn_active')
+                    }
+                })
+                screen.forEach((item) => {
+                    item.style.display = item.style.display === 'none' ? 'flex' : 'none';
+                })
+            }); 
+        })  
+    };
+
+    changeScreen(changeScreenBtn, screen)
+    showMenu(profileButton, profileMenu)
+}); 
